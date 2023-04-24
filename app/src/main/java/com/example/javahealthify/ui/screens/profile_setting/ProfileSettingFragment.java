507a08cff99c4978a9fba2b5260afd19e30ce6c6
Vũ Đức Minh <vuducminh210503@gmail.com>
2023-04-24 16:20:59 +0700

    feat: finish profile navigation

--- app/src/main/java/com/example/javahealthify/ui/screens/profile_setting/ProfileSettingFragment.java
+++ app/src/main/java/com/example/javahealthify/ui/screens/profile_setting/ProfileSettingFragment.java
@@ -3,64 +3,54 @@ package com.example.javahealthify.ui.screens.profile_setting;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.javahealthify.R;
+import com.example.javahealthify.databinding.FragmentProfileSettingBinding;
+import com.example.javahealthify.ui.screens.profile.ProfileFragment;
+import com.example.javahealthify.ui.screens.profile_calories_history.ProfileCaloriesHistoryFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileSettingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ProfileSettingFragment extends Fragment {
+    private FragmentProfileSettingBinding binding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileSettingFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileSettingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileSettingFragment newInstance(String param1, String param2) {
-        ProfileSettingFragment fragment = new ProfileSettingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_setting, container, false);
+        binding = FragmentProfileSettingBinding.inflate(inflater,container,false);
+        binding.linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController navController = NavHostFragment.findNavController(ProfileSettingFragment.this);
+                navController.popBackStack();
+            }
+        });
+
+        binding.changePassBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ProfileSettingFragment.this).navigate(R.id.action_profileSettingFragment_to_profileChangePassFragment2);
+            }
+        });
+
+        binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ProfileSettingFragment.this).navigate(R.id.action_profileSettingFragment_to_signInFragment);
+            }
+        });
+        return binding.getRoot();
     }
 }
