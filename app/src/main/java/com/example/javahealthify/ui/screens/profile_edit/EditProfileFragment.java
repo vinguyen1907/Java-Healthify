b2b301648a46459056ed2fb3a37bc5743275ce44
Vũ Đức Minh <vuducminh210503@gmail.com>
2023-05-03 17:12:54 +0700

    feat: edit profile

--- app/src/main/java/com/example/javahealthify/ui/screens/profile_edit/EditProfileFragment.java
+++ app/src/main/java/com/example/javahealthify/ui/screens/profile_edit/EditProfileFragment.java
@@ -11,6 +11,7 @@ import androidx.navigation.fragment.NavHostFragment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,18 +27,36 @@ import com.example.javahealthify.ui.screens.profile_calories_history.ProfileCalo
 import com.example.javahealthify.ui.screens.profile_personal_info.ProfilePersonalInfoVM;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class EditProfileFragment extends Fragment {
 
     private EditProfileVM editProfileVM;
+
+    private MainVM viewModel;
+
+
+    Boolean isValidName = true;
+    Boolean isValidEmail = true;
+    Boolean isValidDay = true;
+    Boolean isValidPhone = true;
+    Boolean isValidAddress = true;
     private FragmentEditProfileBinding binding;
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -57,7 +76,7 @@ public class EditProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentEditProfileBinding.inflate(inflater,container,false);
+        binding = FragmentEditProfileBinding.inflate(inflater, container, false);
 
         binding.editnameEdt.setText(((NormalUser) user).getName());
         binding.editemailEdt.setText(((NormalUser) user).getEmail());
@@ -75,20 +94,26 @@ public class EditProfileFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                String input = charSequence.toString();
-
-                if (binding.editnameEdt.getText().equals(user.getName())) {
-                    binding.tickIcon1.setVisibility(View.GONE);
-                } else {
-                    binding.tickIcon1.setVisibility(View.VISIBLE);
-                }
 
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-
+                String newName = binding.editnameEdt.getText().toString();
+                if (!newName.equals(user.getName())) {
+                    if (newName.isEmpty()) {
+                        binding.tickIcon1.setVisibility(View.GONE);
+                        isValidName = false;
+                    } else {
+                        binding.tickIcon1.setVisibility(View.VISIBLE);
+                        isValidName = true;
+                    }
+                } else {
+                    binding.tickIcon1.setVisibility(View.GONE);
+                    isValidName = true;
+                }
             }
+
         });
         binding.editemailEdt.addTextChangedListener(new TextWatcher() {
             @Override
@@ -97,18 +122,27 @@ public class EditProfileFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                String input = charSequence.toString();
-
-                if (binding.editemailEdt.getText().equals(user.getEmail())) {
-                    binding.tickIcon2.setVisibility(View.GONE);
-                } else {
-                    binding.tickIcon2.setVisibility(View.VISIBLE);
-                }
 
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
+                String newEmail = binding.editemailEdt.getText().toString();
+                if (!newEmail.equals(user.getEmail())) {
+                    if (binding.editemailEdt.getText().toString().isEmpty()) {
+                        binding.tickIcon2.setVisibility(View.GONE);
+                        isValidEmail = false;
+                    } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.editemailEdt.getText().toString()).matches()) {
+                        binding.tickIcon2.setVisibility(View.GONE);
+                        isValidEmail = false;
+                    } else {
+                        binding.tickIcon2.setVisibility(View.VISIBLE);
+                        isValidEmail = true;
+                    }
+                } else {
+                    binding.tickIcon2.setVisibility(View.GONE);
+                    isValidEmail = true;
+                }
 
             }
         });
@@ -119,19 +153,27 @@ public class EditProfileFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                String input = charSequence.toString();
-
-                if (binding.editdateEdt.getText().equals(((NormalUser) user).getDateOfBirth())) {
-                    binding.tickIcon3.setVisibility(View.GONE);
-                } else {
-                    binding.tickIcon3.setVisibility(View.VISIBLE);
-                }
 
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
+                String dateStr = formatter.format(((NormalUser) user).getDateOfBirth());
 
+                if (!binding.editdateEdt.getText().toString().equals(dateStr)) {
+                    try {
+                        dateFormat.parse(binding.editdateEdt.getText().toString());
+                    } catch (ParseException e) {
+                        binding.tickIcon3.setVisibility(View.GONE);
+                        isValidDay = false;
+                        return;
+                    }
+                    binding.tickIcon3.setVisibility(View.VISIBLE);
+                    isValidDay = true;
+                } else {
+                    binding.tickIcon3.setVisibility(View.GONE);
+                    isValidDay = true;
+                }
             }
         });
         binding.editphoneEdt.addTextChangedListener(new TextWatcher() {
@@ -141,19 +183,25 @@ public class EditProfileFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                String input = charSequence.toString();
-
-                if (binding.editphoneEdt.getText().equals(((NormalUser) user).getPhone())) {
-                    binding.tickIcon4.setVisibility(View.GONE);
-                } else {
-                    binding.tickIcon4.setVisibility(View.VISIBLE);
-                }
 
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-
+                String newPhone = binding.editphoneEdt.getText().toString();
+                if (!newPhone.equals(((NormalUser) user).getPhone())) {
+                    String phone = binding.editphoneEdt.getText().toString();
+                    if (!Pattern.matches("[0-9]+", phone) || phone.length() < 10) {
+                        binding.tickIcon4.setVisibility(View.GONE);
+                        isValidPhone = false;
+                    } else {
+                        binding.tickIcon4.setVisibility(View.VISIBLE);
+                        isValidPhone = true;
+                    }
+                } else {
+                    binding.tickIcon4.setVisibility(View.GONE);
+                    isValidPhone = true;
+                }
             }
         });
         binding.editaddressEdt.addTextChangedListener(new TextWatcher() {
@@ -163,54 +211,60 @@ public class EditProfileFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                String input = charSequence.toString();
-
-                if (binding.editnameEdt.getText().equals(((NormalUser) user).getAddress())) {
-                    binding.tickIcon5.setVisibility(View.GONE);
-                } else {
-                    binding.tickIcon5.setVisibility(View.VISIBLE);
-                }
 
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-
+                String newAddress = binding.editaddressEdt.getText().toString();
+                if (!newAddress.equals(((NormalUser) user).getAddress())) {
+                    if (binding.editaddressEdt.getText().toString().isEmpty()) {
+                        binding.tickIcon5.setVisibility(View.GONE);
+                        isValidAddress = false;
+                    } else {
+                        binding.tickIcon5.setVisibility(View.VISIBLE);
+                        isValidAddress = true;
+                    }
+                } else {
+                    binding.tickIcon5.setVisibility(View.GONE);
+                    isValidAddress = true;
+                }
             }
         });
 
-        binding.tickIcon1.setOnClickListener(new View.OnClickListener() {
+        binding.editprofileSavebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                updateUserData(user.getUid(), binding.editnameEdt.getText().toString().trim(), binding.editphoneEdt.getText().toString().trim() );
+                if ((binding.tickIcon1.getVisibility() == View.GONE
+                        && binding.tickIcon2.getVisibility() == View.GONE
+                        && binding.tickIcon3.getVisibility() == View.GONE
+                        && binding.tickIcon4.getVisibility() == View.GONE
+                        && binding.tickIcon5.getVisibility() == View.GONE)) {
+                    Toast.makeText(requireContext(), "Vui lòng nhập thông tin cần thay đổi", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (isValidName && isValidEmail && isValidDay && isValidPhone && isValidAddress) {
+                    String dateString = binding.editdateEdt.getText().toString().trim();
+                    SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+                    Date date;
+                    try {
+                        date = format.parse(dateString);
+                    } catch (ParseException e) {
+                        throw new RuntimeException(e);
+                    }
+                    updateUserData(user.getUid(),
+                            binding.editnameEdt.getText().toString().trim(),
+                            binding.editemailEdt.getText().toString().trim(),
+                            date,
+                            binding.editphoneEdt.getText().toString().trim(),
+                            binding.editaddressEdt.getText().toString().trim());
+                } else {
+                    Toast.makeText(requireContext(), "Vui lòng nhập đầy đủ và đúng thông tin", Toast.LENGTH_SHORT).show();
+                    return;
+                }
             }
         });
-//        binding.tickIcon2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                updateUserData(user.getUid(),"email",binding.editemailEdt.getText().toString().trim(), 1);
-//            }
-//        });
-//        binding.tickIcon3.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                updateUserData(user.getUid(),"name",binding.editdateEdt.getText().toString().trim(), 1);
-//            }
-//        });
-//        binding.tickIcon4.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                updateUserData(user.getUid(),"name",binding.editphoneEdt.getText().toString().trim(), 1);
-//            }
-//        });
-//        binding.tickIcon5.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                updateUserData(user.getUid(),"name",binding.editaddressEdt.getText().toString().trim(), 1);
-//            }
-//        });
-
-
 
 
         binding.linearLayout.setOnClickListener(new View.OnClickListener() {
@@ -222,14 +276,30 @@ public class EditProfileFragment extends Fragment {
         });
         return binding.getRoot();
     }
-    public void updateUserData(String userId, String name, String phone) {
+
+    public void updateUserData(String userId, String name, String email, Date dateOfBirth, String phone, String address) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("name", name);
+        data.put("email", email);
+        data.put("dateOfBirth", new Timestamp(dateOfBirth));
+        data.put("phone", phone);
+        data.put("address", address);
+
         db.collection("users")
                 .document(userId)
-                .update("name", name, "phone", phone)
+                .set(data, SetOptions.merge())
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
                         Toast.makeText(getContext(), "Success", Toast.LENGTH_SHORT).show();
+                        binding.tickIcon1.setVisibility(View.GONE);
+                        binding.tickIcon2.setVisibility(View.GONE);
+                        binding.tickIcon3.setVisibility(View.GONE);
+                        binding.tickIcon4.setVisibility(View.GONE);
+                        binding.tickIcon5.setVisibility(View.GONE);
+
+
+
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
@@ -239,4 +309,5 @@ public class EditProfileFragment extends Fragment {
                     }
                 });
     }
+
 }
